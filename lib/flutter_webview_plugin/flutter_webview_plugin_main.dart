3de0b00d1923be24abd4bdfd09c588bcfe317400
hesen <hesen@100tal.com>
2020-04-19 16:53:07 +0800

    hesen:1、验证flutter_webview_plugin在js与flutter双向通信，（验证通过）；

--- lib/flutter_webview_plugin/flutter_webview_plugin_main.dart
+++ lib/flutter_webview_plugin/flutter_webview_plugin_main.dart
@@ -10,11 +10,23 @@ class FlutterWebviewPluginMain extends StatefulWidget {
     return FlutterWebviewPluginMainState();
   }
 }
+final flutterWebviewPlugin = new FlutterWebviewPlugin();
+
+final Set<JavascriptChannel> jsChannels = [
+  JavascriptChannel(
+      // js 通信 bridge 对象，注意定义名称需要满足，正则要求
+      name: 'FlutterJsBridge',
+      onMessageReceived: (JavascriptMessage message) {
+        print('flutter 接收到 js 参数：' + message.message);
+        flutterWebviewPlugin.evalJavascript("flutterInvokeJs('flutter invoke js')");
+      }),
+].toSet();
+
 
 class FlutterWebviewPluginMainState extends State {
   var loadUrl = "http://www.baidu.com";
-
-  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  //android 加载本地html，本地h5，需要放在在assets目录下
+  var localUrl = "file:///android_asset/html/native.html";
 
   // On urlChanged stream
   StreamSubscription<String> _onUrlChanged;
@@ -31,6 +43,13 @@ class FlutterWebviewPluginMainState extends State {
 //      }
       print('onUrlChanged url : $url');
     });
+
+    //网页的加载监听（loading,start,finish可以去查看源码），包括重定向
+    flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      print('onStateChanged state type:' +
+          state.type.toString() +
+          " url: ${state.url} navigationType :  ${state.navigationType}");
+    });
   }
 
   @override
@@ -42,7 +61,9 @@ class FlutterWebviewPluginMainState extends State {
   @override
   Widget build(BuildContext context) {
     return new WebviewScaffold(
-        url: loadUrl,
+        url: localUrl,
+        //定义js和flutter通信channel
+        javascriptChannels: jsChannels,
         appBar: new AppBar(
           title: new Text("Widget webview"),
         ));
